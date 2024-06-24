501498f608a16901392de5d9f65bfffbefe30cc3
MielVandeputte <vandeputte.miel@gmail.com>
2024-06-24 20:45:44 +0200

    Add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, ReactiveFormsModule, FormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'template-filler';
-  inputs: string[] = [];
+    title = 'template-filler';
+    inputs: string[] = [];
 
-  templateForm = new FormGroup({
-    template: new FormControl('')
-  });
-
-  constructor() {
-    this.templateForm.valueChanges.subscribe(value => {
-      const templateExpressions = value.template?.matchAll(/{{(.*)}}/g);
-
-      this.inputs = [];
-      for (const nextVal of templateExpressions ?? []) {
-        nextVal[1] && this.inputs.push(nextVal[1]);
-      }
+    templateForm = new FormGroup({
+        template: new FormControl(''),
     });
-  }
+
+    constructor() {
+        this.templateForm.valueChanges.subscribe((value) => {
+            const templateExpressions = value.template?.matchAll(/{{(.*)}}/g);
+
+            this.inputs = [];
+            for (const nextVal of templateExpressions ?? []) {
+                nextVal[1] && this.inputs.push(nextVal[1]);
+            }
+        });
+    }
 }
